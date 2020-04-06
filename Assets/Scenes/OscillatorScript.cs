8cb9a23734fb6fac73cd8d1ec2a6e4d72e310954
HO-Nemises <ho.nemises@gmail.com>
2020-04-06 16:41:23 +0100

    Added fram independance to Thrust so changed sale factor... Updated Oscillation script to use mathf.sin and removed serialization of movement factor

--- Assets/Scenes/OscillatorScript.cs
+++ Assets/Scenes/OscillatorScript.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 
 public class OscillatorScript : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
 
     // todo rmove form inspector later 
-    
-    [Range(0,1)] [SerializeField] float movementFactor;
+    float movementFactor;
 
     Vector3 startingPos;
     // Start is called before the first frame update
@@ -22,6 +22,13 @@ public class OscillatorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
     }
