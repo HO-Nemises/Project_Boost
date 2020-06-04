fe09db9478605fcda3a78a57b8d4ed30b2edc5e9
HO-Nemises <ho.nemises@gmail.com>
2020-06-04 14:27:04 +0100

    CAm controller and new level

--- Assets/Scripts/Camera_controller.cs
+++ Assets/Scripts/Camera_controller.cs
@@ -30,7 +30,7 @@ public class Camera_controller : MonoBehaviour
     void Update()
     {
         float distance = Vector3.Distance(CarObject.transform.position, Cam_helper.transform.position);
-        float zoom = ((distance * 100) / Helper_Range)/100; //get percentage differnce between current distance and helper_range
+        float zoom = ((distance * 100) / Helper_Range)/100; //get percentage difference between current distance and helper_range
         
         if (distance < Helper_Range)
         {
